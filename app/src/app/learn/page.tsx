f9bb7368943ff13e55c60fc4f5767ce7c50d9a8e
shennagyp-netizen <shennagyp@gmail.com>
2026-09-26 20:50:52 +0300

    refactor: make learning gateway public for MVP

--- app/src/app/learn/page.tsx
+++ app/src/app/learn/page.tsx
@@ -1,30 +1,7 @@
+import App from "../../App";
+
 export const dynamic = "force-dynamic";
 
-import { redirect } from "next/navigation";
-import App from "../../App";
-import { requireCurrentUser } from "../../lib/server/auth";
-import { listCompletionHistoryForUser, listMasteryHistoryForUser } from "../../lib/server/progress";
-
-export default async function LearnPage() {
-  const user = await requireCurrentUser().catch((error) => {
-    if (error instanceof Error && error.message === "Authentication required.") {
-      return null;
-    }
-    throw error;
-  });
-
-  if (!user) {
-    redirect("/sign-in");
-  }
-
-  const completionHistory = await listCompletionHistoryForUser(user.id);
-  const masteryHistory = await listMasteryHistoryForUser(user.id);
-
-  return (
-    <App
-      currentUser={{ email: user.email }}
-      initialCompletionHistory={completionHistory}
-      initialMasteryHistory={masteryHistory}
-    />
-  );
+export default function LearnPage() {
+  return <App />;
 }
